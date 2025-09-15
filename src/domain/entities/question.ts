8a2ad08b77daab01d730c2ce576510dd3a29de33
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-15 12:01:12 -0300

    feat: update Answer and Question entities to use UniqueEntityId for authorId

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,11 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityId } from '../../core/entities/unique-entity-id'
 import type { Slug } from './value-objects/slug'
 
 interface QuestionProps {
   title: string
   content: string
-  authorId: string
+  authorId: UniqueEntityId
   slug: Slug
 }
 
