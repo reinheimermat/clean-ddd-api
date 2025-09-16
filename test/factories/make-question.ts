61ed9aa47b6f764557abb5ccc4016c6de8a76381
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-16 20:17:45 -0300

    feat: update makeQuestion factory to accept an optional UniqueEntityId parameter

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -6,14 +6,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: faker.lorem.sentence(),
-    slug: Slug.create('this-is-a-question'),
-    content: faker.lorem.text(),
-    authorId: new UniqueEntityId('author-1'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('this-is-a-question'),
+      content: faker.lorem.text(),
+      authorId: new UniqueEntityId('author-1'),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
