a206ca694fe1faf8c168508527c90ad2c0f0738a
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-16 20:37:38 -0300

    feat: implement DeleteQuestion use case and corresponding tests; add findById and delete methods to QuestionsRepository

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,40 @@
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { DeleteQuestionUseCase } from './delete-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question by id', async () => {
+    const newQuestion = makeQuestion({}, new UniqueEntityId('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: 'question-1',
+      authorId: newQuestion.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a question from another author', async () => {
+    const newQuestion = makeQuestion({}, new UniqueEntityId('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(() =>
+      sut.execute({
+        questionId: 'question-1',
+        authorId: 'author-2',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
