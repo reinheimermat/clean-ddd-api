e681d794b5d96653082a49a9ac17921486b5fc0a
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-17 17:25:08 -0300

    feat: add tests for FetchQuestionAnswers use case; implement recent and paginated answers fetching

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,56 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: FetchQuestionAnswersUseCase
+
+describe('Fetch Question Answers', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to fetch recent answers', async () => {
+    inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-1') }),
+    )
+    inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-1') }),
+    )
+    inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-1') }),
+    )
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(answers).toEqual([
+      expect.objectContaining({ questionId: new UniqueEntityId('question-1') }),
+      expect.objectContaining({ questionId: new UniqueEntityId('question-1') }),
+      expect.objectContaining({ questionId: new UniqueEntityId('question-1') }),
+    ])
+  })
+
+  it('should be able to fetch paginated question answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      inMemoryAnswersRepository.create(
+        makeAnswer({ questionId: new UniqueEntityId('question-1') }),
+      )
+    }
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(answers).toHaveLength(2)
+    expect(answers).toEqual([
+      expect.objectContaining({ questionId: new UniqueEntityId('question-1') }),
+      expect.objectContaining({ questionId: new UniqueEntityId('question-1') }),
+    ])
+  })
+})
