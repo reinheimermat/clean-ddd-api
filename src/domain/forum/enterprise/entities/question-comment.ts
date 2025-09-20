d5352ead1bafc6555d8ec42c8055741b7c5308ac
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-20 13:10:26 -0300

    feat: refactor AnswerComment and QuestionComment to extend Comment class; remove redundant properties

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,41 +1,12 @@
-import { Entity } from '@/core/entities/entity'
 import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
+import { Comment, type CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityId
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
-  }
-
+export class QuestionComment extends Comment<QuestionCommentProps> {
   static create(
     props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityId,
