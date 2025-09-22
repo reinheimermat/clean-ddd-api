f5d046bc5bce48f4f7757218f823987d34b4cfb2
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-22 17:01:18 -0300

    feat: implement CommentOnAnswer use case and in-memory repository for answer comments

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,34 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answers-comments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on a answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    const { answerComment } = await sut.execute({
+      authorId: '1',
+      content: 'This is a comment',
+      answerId: answer.id.toString(),
+    })
+
+    expect(answerComment.id).toBeTruthy()
+    expect(inMemoryAnswerCommentsRepository.items[0]).toEqual(answerComment)
+  })
+})
