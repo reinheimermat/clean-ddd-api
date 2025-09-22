e48f700bf688bdb5936dac7783e9264349bb5b01
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-22 16:50:18 -0300

    feat: implement CommentOnQuestion use case and in-memory repository for question comments

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,35 @@
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on a question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    const { questionComment } = await sut.execute({
+      authorId: '1',
+      content: 'This is a comment',
+      questionId: question.id.toString(),
+    })
+
+    expect(questionComment.id).toBeTruthy()
+    expect(inMemoryQuestionCommentsRepository.items[0]).toEqual(questionComment)
+  })
+})
