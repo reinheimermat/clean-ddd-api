78218e9ae0507b8232257d0f85b41256413908b2
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-24 10:11:15 -0300

    feat: refactor DeleteAnswerComment use case to return Either type for error handling

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { type Either, left, right } from '@/core/either'
 import type { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 
 interface DeleteAnswerCommentUseCaseRequest {
@@ -5,24 +6,28 @@ interface DeleteAnswerCommentUseCaseRequest {
   answerCommentId: string
 }
 
+type DeleteAnswerCommentUseCaseResponse = Either<string, null>
+
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentRepository: AnswerCommentsRepository) {}
 
   async execute({
     authorId,
     answerCommentId,
-  }: DeleteAnswerCommentUseCaseRequest): Promise<void> {
+  }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
     const answerComment =
       await this.answerCommentRepository.findById(answerCommentId)
 
     if (!answerComment) {
-      throw new Error('Answer comment not found.')
+      return left('Answer comment not found.')
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed.')
+      return left('Not allowed.')
     }
 
     await this.answerCommentRepository.delete(answerComment)
+
+    return right(null)
   }
 }
