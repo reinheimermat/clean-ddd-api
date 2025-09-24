b7b6a5fdf546bfb04e8e17ba2d63acb242bb804d
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-24 11:55:59 -0300

    feat: refactor Question class to extend AggregateRoot for improved entity structure

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
 import { Slug } from './value-objects/slug'
@@ -14,7 +14,7 @@ export interface QuestionProps {
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId
   }
